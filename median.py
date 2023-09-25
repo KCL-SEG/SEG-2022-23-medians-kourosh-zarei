856dcfe09634bf7b61ea267b9d31ba0752e80e1b
kourosh-zarei <126338174+kourosh-zarei@users.noreply.github.com>
2023-09-25 14:44:58 +0100

    feat: prints median w/ custom algo

--- median.py
+++ median.py
@@ -3,10 +3,21 @@
 
 while True:
     try:
-        print("Enter a list of numbers separated by commas: ")
-        numbers = [float(value) for value in input().split(",")]
+        numbers = [
+            float(value)
+            for value in input("Enter a list of numbers separated by commas: ").split(
+                ","
+            )
+        ]
+        numbers.sort()
+        if len(numbers) % 2 == 0:
+            median_low = numbers[round(len(numbers) / 2) - 1]
+            median_high = numbers[round(len(numbers) / 2)]
+            median = (median_high + median_low) / 2
+        else:
+            median = numbers[round(len(numbers) / 2)]
     except ValueError:
         print("Some input could not be converted to a number!")
     else:
         break
-print(numbers)
+print(median)
